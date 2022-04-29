b2337b4897019b2634271d2d4f3faef84441b843
davidgu13 <davidgu1312@gmail.com>
2022-04-29 09:55:00 +0300

    Fixed data and outputs folders paths

--- defaults.py
+++ defaults.py
@@ -2,8 +2,8 @@ import os
 import re
 
 SRC_PATH = os.path.dirname(__file__)
-RESULTS_PATH = os.path.join(SRC_PATH, '../results')
-DATA_PATH = os.path.join(SRC_PATH, '../data/all/task1')
+RESULTS_PATH = os.path.join(SRC_PATH, './Results')
+DATA_PATH = os.path.join(SRC_PATH, '.data/Reinflection')
 EVALM_PATH = os.path.join(SRC_PATH, '../eval_scripts/evalm.py')
 
 ALIGN_SYMBOL = '~'
