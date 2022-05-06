5100fad7e0e0431096aee0b992ba6473ebe225d6
davidgu13 <davidgu1312@gmail.com>
2022-05-06 15:21:08 +0300

    Fixed the results extraction file

--- extract_results.py
+++ extract_results.py
@@ -1,6 +1,6 @@
 # This is a Python3 script for extracting the runs results to an Excel file, including final dev and test accuracies.
 import os
-from os.path import join, isdir
+from os.path import join, isdir, basename
 from itertools import product
 import re
 
@@ -13,31 +13,37 @@ def find_float_in_line(line):
     return float(matches[0])
 
 results_folder = join('.', 'Results')
-analogy_types, seeds, io_formats  = ['None'], [100], ['g_g'] # to be extended
+analogy_types, seeds, io_formats  = ['None'], ['100'], ['g_g'] # to be extended
 
 lang_pos_groups = dict(zip(range(1,16), ['kat_V', 'kat_N', 'fin_ADJ', 'swc_V', 'swc_ADJ', 'fin_V', 'sqi_V', 'hun_V',
                                          'bul_V', 'bul_ADJ', 'lav_V', 'lav_N', 'tur_V', 'tur_ADJ', 'fin_N']))
 excel_results_file = "Test-Results None-100-g-g.xlsx"
 
 def extract_accuracies_from_single_folder(folder):
-    with open(join(folder, 'f.stats'), encoding='utf8') as file:
-        lines = file.read().split('\n')
-        return find_float_in_line(lines[-3]), find_float_in_line(lines[-2])
+    # The folder has the format: Outputs_YYYY-MM-DD hhmmss_lang_pos_training-mode_IO_analogy_seed
+    datetime, lang, pos, training_mode = basename(folder).split("_")[1:5]
+
+    lines = open(join(folder, 'f.stats'), encoding='utf8').read().split('\n')
+    dev_accuracy, test_accuracy = find_float_in_line(lines[-3]), find_float_in_line(lines[-2])
+    return datetime, lang, pos, training_mode, dev_accuracy, test_accuracy
 
 
 def main():
     accuracies = []
-    for analogy, seed, io in product(analogy_types, seeds, io_formats):
-        current_parent_folder = join(results_folder, f"{analogy}_{seed}_{io}")
+    for analogy, seed, io_type in product(analogy_types, seeds, io_formats):
+        current_parent_folder = join(results_folder, f"{analogy}_{seed}_{io_type}")
         assert isdir(current_parent_folder), f"Folder Results doesn't exist!"
 
-        folders = [join(current_parent_folder, folder) for folder in os.listdir(current_parent_folder) if isdir(folder)]
+        folders = [join(current_parent_folder, f) for f in os.listdir(current_parent_folder)
+                   if isdir(join(current_parent_folder, f)) and are_substrings_in_string(f, (analogy, seed, io_type))]
+
         for folder in folders:
-            dev_accuracy, test_accuracy = extract_accuracies_from_single_folder(folder)
-            accuracies.append([dev_accuracy, test_accuracy])
+            datetime, lang, pos, training_mode, dev_accuracy, test_accuracy = extract_accuracies_from_single_folder(folder)
+            accuracies.append([datetime, analogy, seed, lang, pos, training_mode, io_type, dev_accuracy, test_accuracy])
 
     from pandas import DataFrame
-    df = DataFrame(accuracies, columns=['Dev Accuracy', 'Test Accuracy'])
+    df = DataFrame(accuracies, columns=['DateTime', 'AnalogyMode', 'Seed', 'Language', 'POS', 'Form/Lemma',
+                                        'IO mode', 'Dev Accuracy', 'Test Accuracy'])
     df = df.fillna("")
     df.to_excel(excel_results_file)
 
