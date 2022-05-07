4d0027739a355f24109cd5c316e40786077c3de8
davidgu13 <davidgu1312@gmail.com>
2022-05-07 11:30:39 +0300

    generalized excel file name

--- extract_results.py
+++ extract_results.py
@@ -13,11 +13,11 @@ def find_float_in_line(line):
     return float(matches[0])
 
 results_folder = join('.', 'Results')
-analogy_types, seeds, io_formats  = ['None'], ['100'], ['g_g'] # to be extended
+analogy_types, seeds, io_formats  = ['None'], ['100', '42', '21'], ['g_g'] # to be extended
 
 lang_pos_groups = dict(zip(range(1,16), ['kat_V', 'kat_N', 'fin_ADJ', 'swc_V', 'swc_ADJ', 'fin_V', 'sqi_V', 'hun_V',
                                          'bul_V', 'bul_ADJ', 'lav_V', 'lav_N', 'tur_V', 'tur_ADJ', 'fin_N']))
-excel_results_file = "Test-Results None-100-g-g.xlsx"
+excel_results_file = f"Test-Results {'_'.join(analogy_types)}-{'_'.join(seeds)}-{'_'.join(io_formats)}.xlsx"
 
 def extract_accuracies_from_single_folder(folder):
     # The folder has the format: Outputs_YYYY-MM-DD hhmmss_lang_pos_training-mode_IO_analogy_seed
